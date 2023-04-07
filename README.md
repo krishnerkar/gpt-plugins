a6547b3b74085acda5f559fbc14d4f3f06b6da6b
Krish <krishnerkar@gmail.com>
2023-04-07 12:26:03 +0530

    Add ReadME + fix footer links

--- README.md
+++ README.md
@@ -1,38 +1,35 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+<h1 align="center">Welcome to GPT Plugins 👋</h1>
+<p>
+  <img alt="Version" src="https://img.shields.io/badge/version-0.1.0-blue.svg?cacheSeconds=2592000" />
+  <a href="https://twitter.com/https:\/\/twitter.com\/krishnerkar" target="_blank">
+    <img alt="Twitter: https:\/\/twitter.com\/krishnerkar" src="https://img.shields.io/twitter/follow/https:\/\/twitter.com\/krishnerkar.svg?style=social" />
+  </a>
+</p>
 
-## Getting Started
+> A directory of all chatgpt plugins
 
-First, run the development server:
+### 🏠 [Homepage](https://gptplugins.app)
 
-```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
+## Install
+
+```sh
+npm install
 ```
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+## Usage
 
-You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
+```sh
+npm run dev
+```
 
-[API routes](https://nextjs.org/docs/api-routes/introduction) can be accessed on [http://localhost:3000/api/hello](http://localhost:3000/api/hello). This endpoint can be edited in `pages/api/hello.ts`.
+## Author
 
-The `pages/api` directory is mapped to `/api/*`. Files in this directory are treated as [API routes](https://nextjs.org/docs/api-routes/introduction) instead of React pages.
+👤 **Krish Nerkar**
 
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
+* Twitter: [@https:\/\/twitter.com\/krishnerkar](https://twitter.com/https:\/\/twitter.com\/krishnerkar)
+* Github: [@krishnerkar](https://github.com/krishnerkar)
 
-## Learn More
+## Show your support
 
-To learn more about Next.js, take a look at the following resources:
+Give a ⭐️ if this project helped you!
 
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
--- app/submit/page.tsx
+++ app/submit/page.tsx
@@ -35,102 +35,102 @@ export default function Add() {
   };
 
   return (
-    <>
+    <div className="flex flex-col justify-between min-h-screen">
       <Navbar />
-      <div className="flex min-h-full flex-col mt-14 justify-center py-12 sm:px-6 lg:px-8">
-        <div className="sm:mx-auto sm:w-full sm:max-w-md">
-          <Link href="/">
-            <img
-              className="mx-auto h-12 w-auto"
-              src="/logo.svg"
-              alt="Your Company"
-            />
-          </Link>
-          <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
-            Submit a plugin
-          </h2>
-          <p className="mt-2 text-center text-sm text-gray-600">
-            Your plugin will be reviewed by the team before addition{" "}
-          </p>
-        </div>
+      <div>
+        <div className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8">
+          <div className="sm:mx-auto sm:w-full sm:max-w-md">
+            <Link href="/">
+              <img
+                className="mx-auto h-12 w-auto"
+                src="/logo.svg"
+                alt="Your Company"
+              />
+            </Link>
+            <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+              Submit a plugin
+            </h2>
+            <p className="mt-2 text-center text-sm text-gray-600">
+              Your plugin will be reviewed by the team before addition{" "}
+            </p>
+          </div>
 
-        <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-          <div className="bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10">
-            <form
-              onSubmit={handleSubmit}
-              className="space-y-6"
-              action="#"
-              method="POST"
-            >
-              <div>
-                <label
-                  htmlFor="name"
-                  className="block text-sm font-medium leading-6 text-gray-900"
-                >
-                  Name
-                </label>
-                <div className="mt-2">
-                  <input
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                    id="name"
-                    name="name"
-                    type="text"
-                    required
-                    placeholder="Zapier"
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  />
+          <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+            <div className="bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10">
+              <form
+                onSubmit={handleSubmit}
+                className="space-y-6"
+                action="#"
+                method="POST"
+              >
+                <div>
+                  <label
+                    htmlFor="name"
+                    className="block text-sm font-medium leading-6 text-gray-900"
+                  >
+                    Name
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      value={name}
+                      onChange={(e) => setName(e.target.value)}
+                      id="name"
+                      name="name"
+                      type="text"
+                      required
+                      placeholder="Zapier"
+                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                  </div>
                 </div>
-              </div>
-              <div>
-                <label
-                  htmlFor="url"
-                  className="block text-sm font-medium leading-6 text-gray-900"
-                >
-                  Plugin URL
-                </label>
-                <div className="mt-2">
-                  <input
-                    value={url}
-                    onChange={(e) => setUrl(e.target.value)}
-                    id="url"
-                    name="url"
-                    type="url"
-                    required
-                    placeholder="https://nla.zapier.com/.well-known/ai-plugin.json"
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  />
+                <div>
+                  <label
+                    htmlFor="url"
+                    className="block text-sm font-medium leading-6 text-gray-900"
+                  >
+                    Plugin URL
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      value={url}
+                      onChange={(e) => setUrl(e.target.value)}
+                      id="url"
+                      name="url"
+                      type="url"
+                      required
+                      placeholder="https://nla.zapier.com/.well-known/ai-plugin.json"
+                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                  </div>
                 </div>
-              </div>
 
-              <div>
-                <button
-                  disabled={loading}
-                  type="submit"
-                  className="disabled:opacity-50 disabled:pointer-events-none flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
-                  {loading ? (
-                    <div
-                      className="inline-block h-5 w-5 animate-spin rounded-full border-2 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
-                      role="status"
-                    >
-                      <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
-                        Loading...
-                      </span>
-                    </div>
-                  ) : (
-                    <p>Submit Plugin</p>
-                  )}
-                </button>
-              </div>
-            </form>
+                <div>
+                  <button
+                    disabled={loading}
+                    type="submit"
+                    className="disabled:opacity-50 disabled:pointer-events-none flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  >
+                    {loading ? (
+                      <div
+                        className="inline-block h-5 w-5 animate-spin rounded-full border-2 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+                        role="status"
+                      >
+                        <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+                          Loading...
+                        </span>
+                      </div>
+                    ) : (
+                      <p>Submit Plugin</p>
+                    )}
+                  </button>
+                </div>
+              </form>
+            </div>
           </div>
         </div>
+        <Toaster />
       </div>
-      <Toaster />
-      <div className="absolute bottom-0 w-full">
-        <Footer />
-      </div>
-    </>
+      <Footer />
+    </div>
   );
 }
