d123e90e4a5018694c140bdbee1ec63a2fcb5b04
Krish <krishnerkar@gmail.com>
2023-04-10 00:41:14 +0530

    Fix Ignore Build Errors Syntax

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // !! WARN !!
-  // Dangerously allow production builds to successfully complete even if
-  // your project has type errors.
-  // !! WARN !!
-  ignoreBuildErrors: true,
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
   experimental: {
     appDir: true,
   },
